499151976c63e3282e7e38c90106185d4dbd9598
b08-10t <manuel_romero2010@hotmail.com>
2019-10-15 19:01:48 +0200

    Ejercicio1

--- JS/saludo.js
+++ JS/saludo.js
@@ -1,7 +1,7 @@
-alert("Hola mundo");
+alert("Hola mundo! \rQué facil es incluir \'comillas simples\'\ry \"comillas dobles\"");
 //confirm("Mensaje con confirmación");
 //prompt("¿Cuál es tu nombre?");
-document.write("escribiendo en la página directamente");
-console.log("mensaje de información en la consola");
-console.error("mensaje de error");
-console.warn("Mensaje de warning");
+//document.write("escribiendo en la página directamente");
+//console.log("mensaje de información en la consola");
+//console.error("mensaje de error");
+//console.warn("Mensaje de warning");
