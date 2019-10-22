5f0c813a166c70cb6b95c5adb08be2139f06d86b
b08-10t <manuel_romero2010@hotmail.com>
2019-10-22 20:08:51 +0200

    Ejercicio 6

--- JS/ejercicio6.js
+++ JS/ejercicio6.js
@@ -5,7 +5,7 @@ function fecha()
     for(var i=2014;i<=2050;i++)
     {
         var d = new Date("January 1, "+i+" 01:15:00");
-        if(d.getDay==0)
+        if(d.getDay()==0)
         {
             document.write("Año "+d.getFullYear()+"<br>");
 
