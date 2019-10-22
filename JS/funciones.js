3632437f4fba6de1715fa4c5cfe0ef0bd40416cd
b08-10t <manuel_romero2010@hotmail.com>
2019-10-22 19:49:20 +0200

    Ejercicios varios

---
+++ JS/funciones.js
@@ -0,0 +1,49 @@
+/*var num=window.prompt("Escribe un numero","");
+
+function parImpar()
+{
+    if(num%2==0)
+    {
+        document.write("El numero "+num+" es par");
+    }else{
+        document.write("El numero "+num+" es impar");
+    }
+}
+parImpar();
+
+var num2=window.prompt("Escribe un numero","");
+function primo()
+{
+    for(let i=2;i<=num2;i++)
+    {
+        cont=0;
+        for(let j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                cont++;
+            }
+        }
+        if(cont==2)
+        {
+            document.write(i+"<br>");
+        }
+    }
+}
+primo();
+*/
+var cadena=window.prompt("Escribe una cadena","");
+function info()
+{
+    if(cadena==cadena.toLowerCase())
+    {
+        document.write("La cadena esta compuesta solo por minusculas");
+    }else if(candena==cadena.toUpperCase()){
+        document.write("La cadena esta compuesta solo por mayusculas");
+    }else{
+        document.write("La cadena esta compuesta por mayusculas y minusculas");
+        
+    }
+
+}
+info();
