589f1cc9094be8afa33d66cf26325e1f63cde2ab
b08-10t <manuel_romero2010@hotmail.com>
2019-10-22 20:48:06 +0200

    Ejercicio 8

---
+++ JS/ejercicio8.js
@@ -0,0 +1,14 @@
+
+var hour, minutes,seconds;
+setInterval(reloj, 1000);
+function reloj()
+{
+    var d = new Date();
+    hour=d.getHours();
+    minutes=d.getMinutes();
+    seconds=d.getSeconds();
+   
+   
+
+}
+document.getElementById("demo").innerHTML = hour+":"+minutes+":"+seconds;
