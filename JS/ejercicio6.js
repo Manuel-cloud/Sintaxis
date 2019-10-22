d9024366375a67b02eafcf6050b646f6f359f9a7
b08-10t <manuel_romero2010@hotmail.com>
2019-10-22 20:07:04 +0200

    Ejercicio 6

--- JS/ejercicio6.js
+++ JS/ejercicio6.js
@@ -4,7 +4,7 @@ function fecha()
     document.write("Lista de domingos<br>");
     for(var i=2014;i<=2050;i++)
     {
-        var d = new Date(i,0,1,0,0,0,0);
+        var d = new Date("January 1, "+i+" 01:15:00");
         if(d.getDay==0)
         {
             document.write("Año "+d.getFullYear()+"<br>");
