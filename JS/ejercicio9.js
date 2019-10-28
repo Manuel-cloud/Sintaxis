be007abd99dfe7730cebaee0c990cbd0d31df763
b08-10t <manuel_romero2010@hotmail.com>
2019-10-28 15:34:29 +0100

    Ejercicio9 Corregido

--- JS/ejercicio9.js
+++ JS/ejercicio9.js
@@ -1,3 +1,12 @@
+"use strict";
 
+function buildDate (date, month, year) {
+	var resultado = document.getElementById ("resultado");
+	var date = parseInt (date);
+	var month = parseInt (month);
+	var year = parseInt (year);
 
-document.getElementById("demo").innerHTML ;
+	//Revisa la referencia de cada objeto. Hay siempre métodos que resuelven problemas sin tener que reinventarlos.
+	resultado.innerHTML = new Date(year, month, date, 0, 0, 0, 0).toLocaleString();
+}
+
