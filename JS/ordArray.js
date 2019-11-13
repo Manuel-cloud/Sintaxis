4ec99e7d956a37fd30cba9847d470be48bdec505
b08-10t <manuel_romero2010@hotmail.com>
2019-11-13 20:54:38 +0100

    OrdenarArrays

--- JS/ordArray.js
+++ JS/ordArray.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var array=[100,"Europa",3,"Asia",-5,"America",23,"Africa",32,"Oceania",14,2];
 var resultado=document.getElementById("resultado");
 function Ordenar(a, b){
@@ -24,4 +26,53 @@ function init(){
 function init2(){
     resultado.innerHTML=array.sort(Ordenar);
     resultado.innerHTML=array.reverse(array.sort(Ordenar));
+}
+
+
+var person=[ { id: 133, firstName: "Juan Carlos", lastName: "Rolón", email: "Hernn84@hotmail.com" }, 
+{ id: 134, firstName: "César", lastName: "Jáquez", email: "Lorena_Delarosa@hotmail.com" }, 
+{ id: 135, firstName: "Leticia", lastName: "Fuentes", email: "Santiago56@gmail.com" }, 
+{ id: 136, firstName: "Amancio", lastName: "Fuentes",email: "Santiago56@gmail.com" }, 
+{ id: 137, firstName: "Lourdes", lastName: "Armenta", email: "Timoteo42@gmail.com" } ];
+
+// document.getElementById("Buttom3").innerHTML=init3();
+// document.getElementById("Buttom3").onclick=Mostrar;
+function init3(){
+    person.sort(function (a, b) {
+        var x = a.lastName.toLowerCase();
+        var y = b.lastName.toLowerCase();
+        if (x != y) { //Comparamos por apellido si no son iguales.
+            if (x < y) { return 1; } 
+            else { return -1; };
+        } else { //Si son iguales comparamos por nombre.
+            x = a.firstName.toLowerCase();
+            y = b.firstName.toLowerCase();
+            if (x < y) {return 1; }
+            else {return -1; };
+        }
+    })
+    
+    resultado.innerHTML="";
+    person.reverse();
+    for(let p of person)
+    {
+        resultado.innerHTML=resultado.innerHTML+p.id+" "+p.firstName+" "+p.lastName+" <a href='mailto:"+p.email+"'>"+p.email+"</a><br>";
+    }
+}
+
+function init4(){
+    person.sort(function (a, b) {
+        if(a.lastName.valueOf!=b.lastName.valueOf){
+            return (a.lastName.valueOf-b.lastName.valueOf )
+        }
+        else{
+            return (a.firstName.valueOf-b.firstName.valueOf)
+        }
+    })
+    resultado.innerHTML="";
+    person.reverse();
+    for(let p of person)
+    {
+        resultado.innerHTML=resultado.innerHTML+p.id+" "+p.firstName+" "+p.lastName+" <a href='mailto:"+p.email+"'>"+p.email+"</a><br>";
+    }
 }
